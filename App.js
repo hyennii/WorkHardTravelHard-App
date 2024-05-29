9be3f93e4f9a55ff26fd569c43b688e2adb9c24d
hyennii <alqkf@icloud.com>
2024-05-29 20:36:53 +0900

    TextInput

--- App.js
+++ App.js
@@ -1,31 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight } from 'react-native';     
+import React, {useState} from "react";
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable, TextInput } from 'react-native';     
 //TouchableOpacity : 누르는 이벤트를 listen할 준비가 된 view
 //TouchableHighlight : 눌렀을 때 배경색이 바뀌는 등의 설정 가능
+//TouchableWithoutFeedback : 화면의 가장 위에서 일어나는 탭 이벤트를 listen
+//Pressable : TouchableWithoutFeedback의 새 버전이라고 볼 수 있음(더 다양한 속성이 많음)
 import { theme } from "./colors.js";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const travel = () => setWorking(false);    //travel을 호출하면 setWorking(false)
+  const work = () => setWorking(true);    //work를 호출하면 setWorking(true)
+  const onChangeText = (payload) => setText(payload);   //유저가 뭔가 입력하면 setText 실행(payload : 전송되는 데이터나 정보를 지칭)
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>        
+        <TouchableOpacity onPress={work}>             
+          <Text style={{...styles.btnText, color: working ? "white" : theme.grey}}>Work</Text>        
         </TouchableOpacity>
 
-        <TouchableHighlight 
-        underlayColor="#DDD"
-        onPress={() => console.log("pressed")}
-        >                     
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableHighlight>
+        <TouchableOpacity onPress={travel}>                     
+          <Text style={{...styles.btnText, color: !working ? "white" : theme.grey}}>Travel</Text>
+        </TouchableOpacity>
       </View>
+
+      <TextInput
+        onChangeText={onChangeText} 
+        value = {text}
+        placeholder={working ? "할 일을 추가하세요" : "어디로 놀러 갈까요?"} 
+        style={styles.input}
+      ></TextInput>
+
     </View>
   );
 }
 
 // onPress속성 : 유저가 Touchable을 눌렀을 때 실행되는 이벤트
+// 중괄호를 두번쓰고 ... 쓰면 : btnText의 모든 스타일 가져다줌
+// payload 를 사용하는 이유 : 
+  // 1. 데이터의 전달: payload는 이벤트 객체 전체 대신 중요한 데이터(텍스트)를 전달할 때 사용되며, 이는 함수가 어떤 종류의 데이터를 받을지 명확하게 나타냄
+  // 2.명확성: 함수 인자의 이름을 payload로 지정함으로써, 코드 읽는 사람이 해당 함수가 무엇을 기대하는지 더 명확하게 이해할 수 있음
+  // 3.일반적인 관례: 소프트웨어 개발에서 payload는 전송되는 데이터나 정보의 내용을 나타내는 데 자주 사용되며, 이는 특히 API 요청이나 상태 관리에서 자주 보이는 패턴
 
 const styles = StyleSheet.create({
   container: {
@@ -42,5 +59,13 @@ const styles = StyleSheet.create({
     fontSize: 38,
     fontWeight: "600",
     color:"white",
+  },
+  input: {
+    backgroundColor:"white",
+    paddingHorizontal:15,
+    paddingVertical : 20,
+    borderRadius : 30,
+    marginTop : 20,
+    fontSize : 16
   }
 });
